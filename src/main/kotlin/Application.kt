fa0451a0aa3f248f5d921d6dbc2471e9399d67df
rafael-cagliari <rafael.cagliari22@gmail.com>
2025-04-27 17:09:24 -0300

    adds application

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,9 +1,16 @@
 package com
 
 import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 
 fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
+    embeddedServer(
+        Netty,
+        port = 8080,
+        host = "0.0.0.0",
+        module = Application::module
+    ).start(wait = true)
 }
 
 fun Application.module() {
